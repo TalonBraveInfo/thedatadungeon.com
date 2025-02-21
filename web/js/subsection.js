a07a2262647ec23c0fb77ac833141c7f2554db44
Mark Sowden <hogsy@oldtimes-software.com>
2025-02-21 12:08:14 +0000

    further style changes for new update

--- web/js/subsection.js
+++ web/js/subsection.js
@@ -1,12 +1,20 @@
 function show_section(elementName) {
-    var dropdowns = document.getElementsByClassName("dd-subsection");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
+    let dropdowns = document.getElementsByClassName("dd-subsection");
+    for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
             openDropdown.classList.remove('show');
         }
     }
 
+    let buttons = document.getElementsByClassName("dd-sub-menu-button");
+    for (let i = 0; i < buttons.length; ++i) {
+        let button = buttons[i];
+        if (button.classList.contains("dd-sub-menu-button-selected")) {
+            button.classList.remove("dd-sub-menu-button-selected");
+        }
+    }
+
     document.getElementById(elementName).classList.toggle("show");
+    document.getElementById(elementName + '-button').classList.add("dd-sub-menu-button-selected");
 }
